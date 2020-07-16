6627102a7f0508a30174d76ddd47869b99eeea7b
alpaddesai <alpa.gupta@gmail.com>
2020-07-15 22:01:32 -0400

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace MergersDocument
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ViewDocument = new System.Windows.Forms.Button();
+            this.axAcroPDF1 = new AxAcroPDFLib.AxAcroPDF();
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ViewDocument
+            // 
+            this.ViewDocument.Location = new System.Drawing.Point(365, 10);
+            this.ViewDocument.Name = "ViewDocument";
+            this.ViewDocument.Size = new System.Drawing.Size(186, 38);
+            this.ViewDocument.TabIndex = 2;
+            this.ViewDocument.Text = "ViewDocument";
+            this.ViewDocument.UseVisualStyleBackColor = true;
+            this.ViewDocument.Click += new System.EventHandler(this.ViewDocument_Click);
+            // 
+            // axAcroPDF1
+            // 
+            this.axAcroPDF1.AllowDrop = true;
+            this.axAcroPDF1.Enabled = true;
+            this.axAcroPDF1.Location = new System.Drawing.Point(16, 54);
+            this.axAcroPDF1.Name = "axAcroPDF1";
+            this.axAcroPDF1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axAcroPDF1.OcxState")));
+            this.axAcroPDF1.Size = new System.Drawing.Size(884, 580);
+            this.axAcroPDF1.TabIndex = 1;
+            this.axAcroPDF1.Enter += new System.EventHandler(this.axAcroPDF1_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 636);
+            this.Controls.Add(this.ViewDocument);
+            this.Controls.Add(this.axAcroPDF1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxAcroPDFLib.AxAcroPDF axAcroPDF1;
+        private System.Windows.Forms.Button ViewDocument;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MergersDocument
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void axAcroPDF1_Enter(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.ShowDialog();
+
+            axAcroPDF1.src = file.FileName;
+        }
+
+        private void ViewDocument_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog file = new OpenFileDialog();
+            file.ShowDialog();
+
+            axAcroPDF1.src = file.FileName;
+
+        }
+    }
+}
